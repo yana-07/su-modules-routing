b167a65dff03b862e87b4be7bd90687d79957f75
yana.radeva7 <yana.radeva7@gmail.com>
2022-09-29 10:04:51 +0300

    work on customer-list-item & customer-list component

--- src/app/customer/customer-list-item/customer-list-item.component.ts
+++ src/app/customer/customer-list-item/customer-list-item.component.ts
@@ -7,12 +7,33 @@ import { IUser } from '../customer.service';
   styleUrls: ['./customer-list-item.component.css']
 })
 export class CustomerListItemComponent implements OnInit {
+  date: Date = new Date();
+  limit: number = 15;
 
   @Input() customer!: IUser;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.customer.email = 'this-is-special-customer@abv.bg';
+      // this.customer = {
+      //   ...this.customer,
+      //   email: 'this-is-special-customer@abv.bg'
+      // }
+    }, 2000)
   }
 
+  handleExpand(): void {
+    this.limit = Infinity;
+  }
+
+  // transform(value: string, limit: number = 10): string {
+  //   console.log('CustomerListItemComponent#shorten', value)
+  //   if (value.length > limit) {
+  //     return `${value.substring(0, limit - 3)}...`;
+  //   }
+
+  //   return value;
+  // }
 }
